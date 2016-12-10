e2184fbbf0711b96fd82244317774f51ddf6301f
Gashaw1 <akalc2004@yahoo.com>
2016-12-10 18:23:12 -0500

    .

--- Data Structures/src/TwoDimensionArray.java
+++ Data Structures/src/TwoDimensionArray.java
@@ -4,15 +4,15 @@
 
 public class TwoDimensionArray {
 
-    String entry;
     int floor;
     int bedrooms;
     int avelalibeltyOnFloor = 0;
     int prices;
     int[][] rents = {
-            {100, 333},
+            {100, 333, 350,358,390,400},
             {444, 555, 600, 700},
-            {555, 955, 939}
+            {555, 955, 939},
+            {899,898,900,1001, 2008}
     };
 
     public void DisplayAll() {
@@ -29,25 +29,37 @@ public class TwoDimensionArray {
         }
     }
 
-    public void SearchBYPrice() {
+    //
+    public void SearchBYPrice(int inPrice) {
+        int[] originalPrice = new int[5];
+        int[] secondNum = new int[5];
+
+        int afterM =0;
         for (floor = 0; floor < rents.length; ++floor) {
 
             for (bedrooms = 0; bedrooms < rents[floor].length; ++bedrooms) {
                 avelalibeltyOnFloor = rents[floor].length;
 
-                for (int i = 0; i < avelalibeltyOnFloor; i++) {
+                for (int i = 0; i < avelalibeltyOnFloor; i++)
+                {
                     //return rent by floor
                     prices = rents[floor][bedrooms];
+                    originalPrice[i] = prices;
+                    secondNum[i] = prices - inPrice;
 
                 }
 
             }
         }
     }
-
-    public static void main(String[] args) {
+    public void getRange(int[]smalest, int[]heighest)
+    {
+        //
+    }
+    public static void main(String[] args)
+    {
         TwoDimensionArray twoDimensionArray = new TwoDimensionArray();
+        twoDimensionArray.DisplayAll();
 
-        twoDimensionArray.SearchBYPrice();
     }
 }
